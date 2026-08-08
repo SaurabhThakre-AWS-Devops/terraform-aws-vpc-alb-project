4373beeccaa5a22be0591639c961135d37cbbb24
Saurabh Thakre <saurabhthakare0205@gmail.com>
2026-08-08 21:19:54 +0530

    Add AWS Application Load Balancer configuration

---
+++ alb.tf
@@ -0,0 +1,63 @@
+resource "aws_lb" "main" {
+  name               = "terraform-demo-alb"
+  internal           = false
+  load_balancer_type = "application"
+
+  security_groups = [
+    aws_security_group.alb.id
+  ]
+
+  subnets = [
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id
+  ]
+
+  tags = {
+    Name = "terraform-demo-alb"
+  }
+}
+
+resource "aws_lb_target_group" "web" {
+  name     = "terraform-web-targets"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    enabled             = true
+    path                = "/"
+    protocol            = "HTTP"
+    port                = "traffic-port"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+  }
+
+  tags = {
+    Name = "terraform-web-target-group"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web_1" {
+  target_group_arn = aws_lb_target_group.web.arn
+  target_id        = aws_instance.web_1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "web_2" {
+  target_group_arn = aws_lb_target_group.web.arn
+  target_id        = aws_instance.web_2.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web.arn
+  }
+}
