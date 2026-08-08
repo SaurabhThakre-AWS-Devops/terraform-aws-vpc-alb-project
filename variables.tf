dd45a6e3ac000f7ff3ff08c2f5bd4e0b1af673fc
Saurabh Thakre <saurabhthakare0205@gmail.com>
2026-08-08 21:14:13 +0530

    Add AWS region variable and update CIDR blocks

--- variables.tf
+++ variables.tf
@@ -1,6 +1,29 @@
-variable "cidr" {
-  description = "CIDR block for VPC"
+variable "aws_region" {
+  description = "AWS region"
   type        = string
-  default     = "192.168.0.0/16"
+  default     = "ap-south-1"
 }
 
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR for public subnet 1"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_2_cidr" {
+  description = "CIDR for public subnet 2"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
